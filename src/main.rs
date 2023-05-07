94b38e3ea58f2295ba25dc7f953a4f98a1edd24c
Guibibi <bgui34@gmail.com>
2023-05-07 18:20:49 -0400

    Changed app title

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "Space Traders",
         native_options,
         Box::new(|cc| Box::new(space_traders::TemplateApp::new(cc))),
     )
