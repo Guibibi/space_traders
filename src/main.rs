610f130da2c3f7a7f6014b832fcacef998193af9
Guillaume Bedard <bgui34@gmail.com>
2023-05-13 19:05:08 -0400

    Added space_traders_api

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,14 @@
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
-    dotenv().ok(); // Loads the .env file
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
-    let user_token: String = env::var("ACCOUNT_TOKEN").expect("ACCOUNT_TOKEN environement variable not set.");
-    println!("{:?}", user_token);
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "Space Traders",
         native_options,
-        Box::new(|cc| Box::new(space_traders::TemplateApp::new(cc, user_token))),
+        Box::new(|cc| Box::new(space_traders::TemplateApp::new(cc))),
     )
 }
 
